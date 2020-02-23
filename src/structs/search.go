da665063b470e86670d2dcb559f450f082fc0c40
Leela Srinivas Siripurapu <lsiripurapu@vmware.com>
2020-02-23 20:25:15 +0530

    Adding issue filter by days

--- src/structs/search.go
+++ src/structs/search.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/srinav141/go-practice/src/structs/issues"
 )
@@ -18,7 +19,16 @@ func main() {
 
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+		fmt.Printf("#%-5d %9.9s %.55s %.65s\n", item.Number, item.User.Login, item.Title, item.Created)
+
+	}
+	pastDays := -365
+	past := time.Now().AddDate(0, 0, pastDays)
+	fmt.Println("Printing issues older than ", pastDays)
+	for _, item := range result.Items {
+		if item.Created.Before(past) {
+			fmt.Printf("#%-5d %9.9s %.55s %.65s\n", item.Number, item.User.Login, item.Title, item.Created)
+		}
 	}
 
 }
