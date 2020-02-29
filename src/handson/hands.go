7fd8833f62df93d805a38ea297c58b8a2cf586cd
Leela Srinivas Siripurapu <lsiripurapu@vmware.com>
2020-02-29 20:37:23 +0530

    adding toposort

--- src/handson/hands.go
+++ src/handson/hands.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("HI")
+
+	seen := make(map[string]string)
+	seen["one"] = "1"
+	seen["two"] = "2"
+	seen["three"] = "3"
+
+	for k, v := range seen {
+		fmt.Println(k, "-->", v)
+	}
 }
