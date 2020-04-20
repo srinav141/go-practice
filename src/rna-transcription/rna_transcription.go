719305ec1f74e010b62feeac7a172f72da999407
Leela Srinivas Siripurapu <lsiripurapu@vmware.com>
2020-04-20 19:06:43 +0530

    rna mapper

---
+++ src/rna-transcription/rna_transcription.go
@@ -0,0 +1,47 @@
+/*
+package strand implements method to converts DNA to RNA strand
+*/
+package strand
+
+//ToRNA converts DNA strand to RNA
+func ToRNA(dna string) string {
+
+	m := make(map[string]string)
+
+	m["C"] = "G"
+	m["G"] = "C"
+	m["T"] = "A"
+	m["A"] = "U"
+
+	if len(dna) == 0 {
+		return ""
+	}
+	res := ""
+
+	for _, r := range dna {
+		res += m[string(r)]
+	}
+
+	return res
+}
+
+// import (
+// 	"strings"
+// )
+
+// var nucleotides = map[rune]rune{
+// 	'G': 'C',
+// 	'C': 'G',
+// 	'T': 'A',
+// 	'A': 'U',
+// }
+
+// /*
+// ToRNA converts nucleotides of a given DNA string to RNA nucleotides
+// */
+// func ToRNA(dna string) string {
+// 	mapper := func(r rune) rune {
+// 		return nucleotides[r]
+// 	}
+// 	return strings.Map(mapper, dna)
+// }
